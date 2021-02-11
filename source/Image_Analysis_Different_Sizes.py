0c4092632f0a45af4b41a01fc5866c091b8d974c
bes82 <52643290+bes82@users.noreply.github.com>
2021-02-11 12:32:27 -0500

    Update Image_Analysis_Different_Sizes.py

--- source/Image_Analysis_Different_Sizes.py
+++ source/Image_Analysis_Different_Sizes.py
@@ -33,6 +33,8 @@ class CountAdheredBloodCells:
         X = np.zeros((self.alpha*self.beta, 128, 128, 3))
         for ii in range(self.alpha):
             for jj in range(self.beta):
+                print(ii)
+                print(jj)
                 y_slider, x_slider = ii*150, jj*150
                 image = self.channel_image[0+y_slider:150+y_slider, 0+x_slider:150+x_slider,:]
                 X[kk,:,:,:] = cv.resize(image, (128,128), interpolation = cv.INTER_CUBIC).reshape(128,128,3)
